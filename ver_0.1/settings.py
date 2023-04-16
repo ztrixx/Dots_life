b764e1890ef553353fee42ddf45c822d9859f512
ztrixx <130905289+ztrixx@users.noreply.github.com>
2023-04-16 05:04:21 -0700

    Удаление версии 0.1

--- ver_0.1/settings.py
+++
@@ -1,9 +0,0 @@
-import random
-
-
-def random_point():
-    return random.randint(20, SCREEN_SIZE[0] - 20), random.randint(20, SCREEN_SIZE[1] - 20)
-
-
-SCREEN_SIZE = (760,1280)
-ALL_DIRECTIONS = [(0, 3), (0, -3), (3, 0), (-3, 0), (3, 3), (3, -3), (-3, 3), (-3, -3)]
